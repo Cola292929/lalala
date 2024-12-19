25f06a43fdb90a2cdb258cc7ea9f5e6f60b418f8
Womian <84675827@qq.com>
2024-12-20 06:55:01 +0800

    fix

--- scripts/gp4o/script/gtp4ohd.js
+++
@@ -1,9 +0,0 @@
-/*
-脚本引用https://raw.githubusercontent.com/Yu9191/Rewrite/main/GTP4ohd.js
-*/
-if ($request.url.includes('api/img/aiSketch')) {
-    let body = $request.body.replace(/("taskParameter"\s*:\s*)"\d+"/, '$1"0"');
-    $done({ body: body });
-} else {
-    $done({});
-}
